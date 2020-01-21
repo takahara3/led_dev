9d1d4988634ccbfd45ed0b92e2d24b5fa047e9f0
takahara <kantz231017@gmail.com>
2020-01-21 14:22:46 +0900

    fix license

--- led_dev.c
+++ led_dev.c
@@ -2,7 +2,7 @@
  * myled.c
  *
  * Copyright (C) 2019 Ryuichi Ueda <ryuichiueda@gmail.com>
- * Copyright (C) 2019 Kazuki Takahara <s17c1088bm@s.chibakoudai.jp>
+ * Copyright (C) 2020 Kazuki Takahara <s17c1088bm@s.chibakoudai.jp>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
